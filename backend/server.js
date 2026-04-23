a94c8c930e6fdeb0bb46fa53607920c568268ab0
Mohammed-Nur Nur <mohammed-nur.nur0002@stud.hkr.se>
2026-04-23 20:30:46 +0200

    feat: connect to MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const dotenv = require('dotenv')
 dotenv.config()
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 
 app.use(express.json())
 
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('Connected to MongoDB!'))
+.catch((err) => console.log('Error connecting to MongoDB', err))
 app.get('/', (req, res) => {
   res.send('Calorie Tacker API is running')
 })
